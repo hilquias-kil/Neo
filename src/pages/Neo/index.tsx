d9afd80a3a2c9148ff3e8e272e579dd5446c2cd4
hilquias-kil <hilquias.kil@gmail.com>
2022-02-23 16:36:34 -0300

    add approach date

--- src/pages/Neo/index.tsx
+++ src/pages/Neo/index.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 import { useParams } from "react-router";
 import { Link } from "react-router-dom";
+import dayjs from "dayjs";
+import "dayjs/locale/pt-br";
 import useNeo from "../../api/useNeo";
 import Layout from "../../components/Layout";
 import Loading from "../../components/Loading";
 import Error from "../../components/Error";
+
+const formateDate = (date: string) => {
+  let dateFormated = date.replaceAll("%", "de")
+
+  return dateFormated.slice(0,1) + dateFormated.slice(1).toLowerCase();
+};
 
 const Neo: React.FC = () => {
   const { id } = useParams();
@@ -19,8 +27,15 @@ const Neo: React.FC = () => {
       <Link to="/"> Back</Link>
       <h1>{data?.name}</h1>
       <p>{data?.designation}</p>
+      {formateDate(
+        dayjs(data?.close_approach_data[0].close_approach_date_full)
+          .locale("pt-br")
+          .format("dddd, DD % MMMM % YYYY")
+      )}
 
-      <code><pre>{JSON.stringify(data?.orbital_data, null, ' ')}</pre></code>
+      <code>
+        <pre>{JSON.stringify(data?.orbital_data, null, " ")}</pre>
+      </code>
     </Layout>
   );
 };
